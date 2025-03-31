b4602f13fc5fd1642a46a8f482b34b1724bd49ec
levimarkmuigai <levimarkmuigai@gmail.com>
2025-03-31 09:01:40 +0300

    Add styled-components and create theme file with color definitions

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+// src/theme.js
+export const colors = {
+    tanGold: '#d2b48c',
+    darkTanGold: '#c9a671',
+    lightGray: '#f5f5f5',
+    darkBackground: '#2b2b2b',
+    primary: '#333',
+    text: '#333',
+  };
+  
